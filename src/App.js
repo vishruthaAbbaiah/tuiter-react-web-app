f7217fa95978eee5915f32cb046243f7450f68c2
Vishrutha Abbaiah <vishruthaabbaiah@Vishruthas-MacBook-Pro.local>
2022-09-24 11:25:02 -0400

    added change to assignment 2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <h1>
           Welcome to Web Development 2022
         </h1>
+        <h2>This is a branch for assignment 2 </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
